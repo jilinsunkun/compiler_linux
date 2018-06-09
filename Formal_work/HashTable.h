a88852360d9997bc774a0fa723311ca2974479f7
jilinsunkun <2589095800@qq.com>
2018-06-09 23:20:01 +0800

    auto commit

--- Formal_work/HashTable.h
+++ Formal_work/HashTable.h
@@ -1,12 +1,8 @@
 #include <stdio.h>
 int  yylex  (void);
 void  yyerror  (const char *str);
-
-
-int isShouldAdd = 0;
 int keyId = 1;
-int depth = 0;
-
+int itemDepth = 0;
 
 char jasm[10000];
 char temp_parameter[1000];
@@ -17,79 +13,139 @@ int temp_fun_index = 0;
 int is_print = 0;
 int is_assigning = 0;
 
-#define SIZE 1024
-#define MAX_LINE_LENG 1024
-
 struct DataItem {
-   char idName[100];
-   char type[100];
-   char value[100];
-   int key;
-   int depth;
+  char idName[100];
+  char type[100];
+  char value[100];
+  int key;
+  int depth;
+  int fun_index;
 };
 
-struct DataItem* *hashArray;
+struct DataItem * *hashArray;
 
 //Creates a symbol table.
-struct DataItem* * create(){
+struct DataItem * * create() {
   static struct DataItem* newHashArray[SIZE] ;
   return newHashArray;
 };
+
 //Returns index of the entry for int key
 int lookup(char *idName , int depth) {
-   int hashIndex = 1;
-   int isFind = 0;
-   while(hashArray[hashIndex] != NULL) {
-      if(strcmp(hashArray[hashIndex]->idName, idName) == 0 && hashArray[hashIndex]->depth == depth)
-      {
-        isFind = 1;
-        break;
-      }
-      else
-      {
-        ++hashIndex;
-      }
-   }
-   if(isFind == 1){
-     return hashIndex;
-   }
-   else{
-     return -1;
-   }
+  int hashIndex = 1;
+  int isFind = 0;
+  int first_index = 0;
+
+  while (hashArray[hashIndex] != NULL) {
+    if ( hashArray[hashIndex]->depth == depth && hashArray[hashIndex]->fun_index == temp_fun_index)
+    {
+      first_index = hashIndex;
+      break;
+    }
+    else
+    {
+      ++hashIndex;
+    }
+  }
+
+  while (hashArray[hashIndex] != NULL) {
+    if (strcmp(hashArray[hashIndex]->idName, idName) == 0 && hashArray[hashIndex]->depth == depth && hashArray[hashIndex]->fun_index == temp_fun_index && strcmp(hashArray[hashIndex]->type, "const") != 0)
+    {
+      isFind = 1;
+      break;
+    }
+    else
+    {
+      ++hashIndex;
+    }
+  }
+  if (isFind == 1) {
+    return hashIndex - first_index;
+  }
+  else {
+    return -1;
+  }
 }
+
+// search by relaxed condition
+int lookup_nodepth(char *idName) {
+  int hashIndex = 1;
+  int isFind = 0;
+
+  while (hashArray[hashIndex] != NULL) {
+    if (strcmp(hashArray[hashIndex]->idName, idName) == 0 )
+    {
+      isFind = 1;
+      break;
+    }
+    else
+    {
+      ++hashIndex;
+    }
+  }
+  if (isFind == 1) {
+    return hashIndex;
+  }
+  else {
+    return -1;
+  }
+}
+
+// search for const
+char* lookup_const(char *idName) {
+  int hashIndex = 1;
+  int isFind = 0;
+  int depth = 0;
+
+  while (hashArray[hashIndex] != NULL) {
+      if (strcmp(hashArray[hashIndex]->idName, idName) == 0 && hashArray[hashIndex]->depth == depth && hashArray[hashIndex]->fun_index == temp_fun_index 
+        && strcmp(hashArray[hashIndex]->type, "const") == 0)
+    {
+      isFind = 1;
+      break;
+    }
+    else
+    {
+      ++hashIndex;
+    }
+  }
+  if (isFind == 1) {
+    return hashArray[hashIndex]->value;
+  }
+  else {
+    return "";
+  }
+}
+
 //Inserts s into  the symbol table
 void insert(char *idName , char *type, char *value) {
+  struct DataItem *item = (struct DataItem*) malloc(sizeof(struct DataItem));
+  strcpy(item->idName, idName);
+  strcpy(item->type, type);
+  strcpy(item->value, value);
+  item->depth = itemDepth;
+  item->fun_index = function_index;
+  item->key = keyId;
 
-     struct DataItem *item = (struct DataItem*) malloc(sizeof(struct DataItem));
-     strcpy(item->idName, idName);
-     strcpy(item->type, type);
-    strcpy(item->value, value);
-    item->depth = depth;
-      item->key = keyId;
-
-    hashArray[keyId] = item;
-    keyId ++;
-}
-
-void clear(){
-   for(int i = 1;i < SIZE;i++)
-   {
-    if(hashArray[i] != NULL)
-      {
-          hashArray[i] = NULL;
-      }
-  }
+  hashArray[keyId] = item;
+  keyId ++;
 }
 
 //Dumps all entries of the symbol table. returns index of the entry.
 void dump() {
-   for(int i = 1;i < SIZE;i++)
-   {
-    if(hashArray[i] != NULL)
-      {
-        printf("%-*d:%-*s%-*s%-*s%-*d%-*s\n", 5, i, 20, hashArray[i]->idName, 15, hashArray[i]->type, 15, hashArray[i]->value, 5, hashArray[i]->depth,10);
-
-      }
+  for (int i = 1; i < SIZE; i++)
+  {
+    if (hashArray[i] != NULL)
+    {
+      printf("%-*d:%-*s%-*s%-*s%-*d%-*d\n", 5, i, 15, hashArray[i]->idName, 15, hashArray[i]->type, 25, hashArray[i]->value, 15, hashArray[i]->fun_index, 5, hashArray[i]->depth);
+    }
   }
-  clear();
+  // clear the hashArray
+  for (int i = 1; i < SIZE; i++)
+  {
+    if (hashArray[i] != NULL)
+      {
+    hashArray[i] = NULL;
+    }
+  }
 }
