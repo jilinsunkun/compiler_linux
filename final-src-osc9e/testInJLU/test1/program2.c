c3aee0e90091932e76d7e8cffe0e749d4498591d
jilinsunkun <2589095800@qq.com>
2018-08-30 15:00:01 +0800

    auto commit

--- final-src-osc9e/testInJLU/test1/program2.c
+++ final-src-osc9e/testInJLU/test1/program2.c
@@ -17,5 +17,33 @@ main(int argc, char **argv)
 {
 	pthread_mutex_init(&mutex,NULL);
 	sem_init(&product,0,0);
-	sem_init(&warehouse,0,8)
+	sem_init(&warehouse,0,8);
+	int clone_flag,arg,retval;
+	char *stack;
+	clone_flag=CLONE_VM|CLONE_SIGHAND|CLONE_FS|CLONE_FILES;
+	int i;
+	for (int i = 0; i < 2; ++i)
+	{
+		arg=i;
+		stack=(char*)malloc(4069);
+		retval=clone((void *)product,&(stack[4095]),clone_flag,(void *)&arg);
+		stack=(char *)malloc(4096);
+		retval=clone((void *)consumer,&(stack[4095]),clone_flag,(void *)&arg);
+		sleep(1);
+
+		/* code */
+	}
+	exit(1);
+}
+int producter(void *args)
+{
+	int id =*((int*)args);
+	int i;
+	for (int i = 0; i < 10; ++i)
+	{
+		sleep(i+1);
+		sem_wait(&warehouse);
+		pthread_mutex_lock(&mutex);
+		/* code */
+	}
 }
