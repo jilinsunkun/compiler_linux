39a5455912cd1ad633060d9dcffd1fd6f018516a
jilinsunkun <2589095800@qq.com>
2018-09-06 13:30:01 +0800

    auto commit

--- final-src-osc9e/testInJLU/test2/2_1.c
+++ final-src-osc9e/testInJLU/test2/2_1.c
@@ -60,6 +60,55 @@ int main(int argc, char **argv)
 		tasks[i].arg=i;
 		tasks[i].call_num=1;
 		sum=sum+(float)tasks[i].ci/(float)tasks[i].ti;
+		printf("please input algorithm 1 for EDF 2 for RMS:\n");
+		getchar();
+		scanf("%d",&alg);
+		printf("please inpiut the demo time:\n");
+		scanf("%d",&demo_time);
+		double r=1;
+		if(alg==2){
+			/*RMS*/
+			r=((double)task_num)*(exp(log(2)/(double)task_num)-1);
+			printf("r is %1f\n", );
+		}
+		if(sum>r){
+			printf("sum=%1f>r=%1f,not schedulable!\n",sum_r );
+			exit(2)
+		}
+		pthread_creat(&idle_proc,NULL,(void*)idle,NULL);
+		for (int i = 0; i < task_num; ++i)
+		{
+			pthread_creat(&task[i].th,NULL,(void *)proc,&task[i].arg);
+			/* code */
+		}
+		for (int i = 0; i < demo_time; ++i)
+		{
+			int j;
+			if ((curr_proc=select_proc(alg))!=-1)
+			{
+				pthread_mutex_unlock(&proc_wait[curr_proc]);
+				pthread_mutex_lock(&main_wait);
+				/* code */
+			}
+			else{
+				pthread_mutex_unlock(idle_wait);
+				pthread_mutex_lock(&main_wait);
+			}
+			for (int i = 0; i < task_num; ++i)
+			{
+				if(--tasks[j].ti_left==0){
+					tasks[j].ti_left=tasks[j].ti;
+					tasks[j].ci_left=tasks[j].ci;
+					pthread_creat(&tasks[j].th,NULL,(void *)proc,&tasks[j].arg);
+					tasks[j].flag=2;
+				}
+				/* code */
+			}
+
+			/* code */
+		}
+		printf("\n");
+		sleep(10);
 		/* code */
 	}
 	/* code */
